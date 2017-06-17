99d8fe7cd8bbd56d13ac9cad9b2431ebb52129b0
User <User@WIN-RP7S6D11T38>
2017-06-17 13:53:49 +0300

    user bean

--- StockPlatform/src/com/bigroi/stock/bean/User.java
+++ StockPlatform/src/com/bigroi/stock/bean/User.java
@@ -1,7 +1,37 @@
 package com.bigroi.stock.bean;
 
 public class User {
+	
 	private long id;
 	private String login;
 	private String password;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", login=" + login + ", password=" + password + "]";
+	}
 }
