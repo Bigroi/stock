c39ef6fc5350dbebcbd9148a383c8ebdf99031e5
User <User@1programmist>
2017-06-19 16:05:49 +0200

    AppContext

--- StockPlatform/src/com/bigroi/stock/controller/AppContext.java
+++ StockPlatform/src/com/bigroi/stock/controller/AppContext.java
@@ -4,9 +4,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppContext {
+	private static final ApplicationContext CONTEX = new ClassPathXmlApplicationContext("spring.xml");
 	
-	public static ApplicationContext getContext(){
-		
-		return new ClassPathXmlApplicationContext("spring.xml");		
+	public static ApplicationContext getContext(){		
+		return CONTEX;		
 	}
 }
