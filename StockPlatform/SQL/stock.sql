3ed2f5957f889ee6d0017dfd79ef60d1fb60f379
evgen <evgen@192.168.100.48>
2017-06-24 18:30:02 +0300

    update DB

--- StockPlatform/SQL/stock.sql
+++ StockPlatform/SQL/stock.sql
@@ -15,25 +15,6 @@
 CREATE DATABASE IF NOT EXISTS `stock` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `stock`;
 
--- Дамп структуры для таблица stock.application
-CREATE TABLE IF NOT EXISTS `application` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `description` varchar(1000) NOT NULL DEFAULT '0',
-  `product` bigint(20) NOT NULL DEFAULT '0',
-  `max_price` decimal(10,0) NOT NULL DEFAULT '0',
-  `customer` bigint(20) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `FK_application_product` (`product`),
-  KEY `FK_application_company` (`customer`),
-  CONSTRAINT `FK_application_company` FOREIGN KEY (`customer`) REFERENCES `company` (`id`),
-  CONSTRAINT `FK_application_product` FOREIGN KEY (`product`) REFERENCES `product` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Дамп данных таблицы stock.application: ~0 rows (приблизительно)
-DELETE FROM `application`;
-/*!40000 ALTER TABLE `application` DISABLE KEYS */;
-/*!40000 ALTER TABLE `application` ENABLE KEYS */;
-
 -- Дамп структуры для таблица stock.archive
 CREATE TABLE IF NOT EXISTS `archive` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -49,13 +30,9 @@ CREATE TABLE IF NOT EXISTS `archive` (
   CONSTRAINT `FK_archive_company` FOREIGN KEY (`saler`) REFERENCES `company` (`id`),
   CONSTRAINT `FK_archive_company_2` FOREIGN KEY (`customer`) REFERENCES `company` (`id`),
   CONSTRAINT `FK_archive_product` FOREIGN KEY (`product`) REFERENCES `product` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы stock.archive: ~0 rows (приблизительно)
-DELETE FROM `archive`;
-/*!40000 ALTER TABLE `archive` DISABLE KEYS */;
-/*!40000 ALTER TABLE `archive` ENABLE KEYS */;
-
+-- Экспортируемые данные не выделены.
 -- Дамп структуры для таблица stock.blacklist
 CREATE TABLE IF NOT EXISTS `blacklist` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -64,35 +41,25 @@ CREATE TABLE IF NOT EXISTS `blacklist` (
   PRIMARY KEY (`id`),
   KEY `FK_blacklist_application` (`app_id`),
   KEY `FK_blacklist_lot` (`lot_id`),
-  CONSTRAINT `FK_blacklist_application` FOREIGN KEY (`app_id`) REFERENCES `application` (`id`),
+  CONSTRAINT `FK_blacklist_application` FOREIGN KEY (`app_id`) REFERENCES `tender` (`id`),
   CONSTRAINT `FK_blacklist_lot` FOREIGN KEY (`lot_id`) REFERENCES `lot` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы stock.blacklist: ~0 rows (приблизительно)
-DELETE FROM `blacklist`;
-/*!40000 ALTER TABLE `blacklist` DISABLE KEYS */;
-/*!40000 ALTER TABLE `blacklist` ENABLE KEYS */;
-
+-- Экспортируемые данные не выделены.
 -- Дамп структуры для таблица stock.company
 CREATE TABLE IF NOT EXISTS `company` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL DEFAULT '0',
   `email` varchar(50) NOT NULL DEFAULT '0',
-  `phone` int(50) NOT NULL DEFAULT '0',
+  `phone` varchar(50) NOT NULL DEFAULT '0',
   `reg_number` varchar(100) NOT NULL DEFAULT '0',
   `country` varchar(50) NOT NULL DEFAULT '0',
   `city` varchar(50) NOT NULL DEFAULT '0',
-  `user_id` bigint(20) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы stock.company: ~0 rows (приблизительно)
-DELETE FROM `company`;
-/*!40000 ALTER TABLE `company` DISABLE KEYS */;
-/*!40000 ALTER TABLE `company` ENABLE KEYS */;
-
+-- Экспортируемые данные не выделены.
 -- Дамп структуры для таблица stock.lot
 CREATE TABLE IF NOT EXISTS `lot` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -106,39 +73,45 @@ CREATE TABLE IF NOT EXISTS `lot` (
   KEY `product` (`poduct`),
   CONSTRAINT `product` FOREIGN KEY (`poduct`) REFERENCES `product` (`id`),
   CONSTRAINT `saler` FOREIGN KEY (`saler`) REFERENCES `company` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы stock.lot: ~0 rows (приблизительно)
-DELETE FROM `lot`;
-/*!40000 ALTER TABLE `lot` DISABLE KEYS */;
-/*!40000 ALTER TABLE `lot` ENABLE KEYS */;
-
+-- Экспортируемые данные не выделены.
 -- Дамп структуры для таблица stock.product
 CREATE TABLE IF NOT EXISTS `product` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `description` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы stock.product: ~0 rows (приблизительно)
-DELETE FROM `product`;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+-- Экспортируемые данные не выделены.
+-- Дамп структуры для таблица stock.tender
+CREATE TABLE IF NOT EXISTS `tender` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(1000) NOT NULL DEFAULT '0',
+  `product` bigint(20) NOT NULL DEFAULT '0',
+  `max_price` decimal(10,0) NOT NULL DEFAULT '0',
+  `customer` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK_application_product` (`product`),
+  KEY `FK_application_company` (`customer`),
+  CONSTRAINT `FK_application_company` FOREIGN KEY (`customer`) REFERENCES `company` (`id`),
+  CONSTRAINT `FK_application_product` FOREIGN KEY (`product`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Экспортируемые данные не выделены.
 -- Дамп структуры для таблица stock.user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `login` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `companyId` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_user_company` (`companyId`),
+  CONSTRAINT `FK_user_company` FOREIGN KEY (`companyId`) REFERENCES `company` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы stock.user: ~0 rows (приблизительно)
-DELETE FROM `user`;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-
+-- Экспортируемые данные не выделены.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
