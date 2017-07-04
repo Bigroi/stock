b63e7593e28d2e2d0f01960efc789eed8eb75d60
Александр <User@1programmist>
2017-07-04 10:43:37 +0200

    messageFromFile.read(string) - чтение сообщение из файла .на вход подается имя файла, а на выходе мапа с 2мя ключами "subject" - Тема cообщения( первая строка файла), "text" - само сообщение (остальные строки). Чтобы нормально поддерживались символы национальных алфавитов, файл должен быть в формате UTF-8. Пока оставил main как пример

---
+++ StockPlatform/src/com/bigroi/stock/util/messageFromFile.java
@@ -0,0 +1,37 @@
+package com.bigroi.stock.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class messageFromFile {
+
+	public static Map<String, String> read(String fileName) throws IOException {
+		Map<String, String> map = new HashMap<>();
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+				Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName),
+				StandardCharsets.UTF_8))) {
+			String subject = "";
+			String text = "";
+			boolean flag = true;
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (flag) {
+					subject = line;
+					flag = false;
+				} else {
+					text = text + line + System.lineSeparator();
+				}
+			}
+			map.put("subject", subject);
+			if (text.endsWith(System.lineSeparator())) {
+				text = text.substring(0, text.length() - 2);
+			}
+			map.put("text", text);
+		}
+		return map;
+	}
+}
