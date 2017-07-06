ffe58a09dd5d6a0b8cf0d3ede3d18de060369695
evgen <admin@192.168.60.78>
2017-07-06 14:02:41 +0300

    MailManager

---
+++ StockPlatform/junitTest/com/bigroi/stock/mailtest/send/MailManagerTest.java
@@ -0,0 +1,22 @@
+package com.bigroi.stock.mailtest.send;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.bigroi.stock.mail.MailManager;
+
+public class MailManagerTest {
+	
+	private static  MailManager send;
+	
+	@BeforeClass
+	public static void init(){
+		send =  new MailManager("your email","your passw");
+	}
+	
+	
+	@Test
+	public  void send(){
+		send.send("your email", "test", "hello world!");
+	}
+}
---
+++ StockPlatform/src/com/bigroi/stock/mail/MailManager.java
@@ -0,0 +1,50 @@
+package com.bigroi.stock.mail;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailManager {
+
+	private String user;
+	private String password;
+	private Properties properties;
+
+	public MailManager(String user, String password) {
+		this.user = user;
+		this.password = password;
+
+		properties = new Properties();
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");// port TLS 
+		properties.put("mail.mime.charset", "UTF-8");
+	}
+
+	public void send(String toEmail, String subject, String text) {
+		Session session = Session.getInstance(properties, new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(user, password);
+			}
+		});
+		
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(user));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+			message.setSubject(subject);
+			message.setText(text);
+			Transport.send(message);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
