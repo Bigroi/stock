056ae7aec7b1080514ec1d8eb50a96381a02f964
Александр <User@1programmist>
2017-07-10 11:42:28 +0200

    Ссылки п проперти файл. link.properties

---
+++ StockPlatform/src/com/bigroi/stock/bean/Link.java
@@ -0,0 +1,20 @@
+package com.bigroi.stock.bean;
+
+public class Link {
+	private String sellerConfirmationLink;
+	private String customerConfirmationLink;
+	
+	public String getSellerConfirmationLink() {
+		return sellerConfirmationLink;
+	}
+	public void setSellerConfirmationLink(String sellerConfirmationLink) {
+		this.sellerConfirmationLink = sellerConfirmationLink;
+	}
+	public String getCustomerConfirmationLink() {
+		return customerConfirmationLink;
+	}
+	public void setCustomerConfirmationLink(String customerConfirmationLink) {
+		this.customerConfirmationLink = customerConfirmationLink;
+	}
+
+}
--- StockPlatform/src/com/bigroi/stock/trade/EmailForConfirmation.java
+++ StockPlatform/src/com/bigroi/stock/trade/EmailForConfirmation.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bigroi.stock.bean.Link;
 import com.bigroi.stock.bean.Lot;
 import com.bigroi.stock.bean.PreDeal;
 import com.bigroi.stock.bean.Tender;
@@ -21,12 +25,14 @@ import com.bigroi.stock.util.MessageFromFile;
 public class EmailForConfirmation {
 	private final static String CUSTOMER_CONFIRMATION_FILE = "customerConfirmationUTF8.txt";
 	private final static String SELLER_CONFIRMATION_FILE = "sellerConfirmationUTF8.txt";
-	private final static String SELLER_CONFIRMATION_LINK = "http://localhost:8080/StockPlatform/SellerCheck.spr?";
-	private final static String CUSTOMER_CONFIRMATION_LINK = "http://localhost:8080/StockPlatform/CustomerCheck.spr?";
 	
-	public void send() throws IOException, DaoException {
+	private static final ApplicationContext CONTEX = new ClassPathXmlApplicationContext("spring.xml");
+	
+	public void send() throws IOException, DaoException {		
 		Map<MessagePart, String> customerMessage = MessageFromFile.read(CUSTOMER_CONFIRMATION_FILE);
 		Map<MessagePart, String> sellerMessage = MessageFromFile.read(SELLER_CONFIRMATION_FILE);
+		Link link = (Link) CONTEX.getBean("link");
+		
 		List<PreDeal> preDials = DaoFactory.getPreDealDao().getAllPreDeal();
 		String sellerSubject = sellerMessage.get(MessagePart.SUBJECT);
 		String customerSubject = customerMessage.get(MessagePart.SUBJECT);
@@ -34,16 +40,16 @@ public class EmailForConfirmation {
 		for (PreDeal preDeal : preDials) {
 			Lot lot = DaoFactory.getLotDao().getById(preDeal.getLotId());
 			String sellerEmail = DaoFactory.getCompanyDao().getById(lot.getSellerId()).getEmail();
-
+			
 			Tender tender = DaoFactory.getTenderDao().getById(preDeal.getTenderId());
 			String customerEmail = DaoFactory.getCompanyDao().getById(tender.getCustomerId()).getEmail();			
 			
 			String sellerText = sellerMessage.get(MessagePart.TEXT);
 			String customerText = customerMessage.get(MessagePart.TEXT);
 
-			String sellerLink = SELLER_CONFIRMATION_LINK + "id=" + preDeal.getId() + "&key="
+			String sellerLink = link.getSellerConfirmationLink() + "id=" + preDeal.getId() + "&key="
 					+ preDeal.getSellerHashCode() + "&action=";
-			String customerLink = CUSTOMER_CONFIRMATION_LINK + "id=" + preDeal.getId() + "&key="
+			String customerLink = link.getCustomerConfirmationLink() + "id=" + preDeal.getId() + "&key="
 					+ preDeal.getCustomerHashCode() + "&action=";
 
 			String sellerLinkApprove = sellerLink + Action.APPROVE;
@@ -56,19 +62,20 @@ public class EmailForConfirmation {
 			double price = (minPrace + maxPrice) / 2;
 
 			sellerText = sellerText
-			.replaceAll("@lotId", preDeal.getLotId() + "")
-			.replaceAll("@lotDate", lot.getDateStr())
-			.replaceAll("@lotPrice", lot.getMinPrice() + "")
-			.replaceAll("@price", price + "")
-			.replaceAll("@sellerLinkApprove", sellerLinkApprove)
-			.replaceAll("@sellerLinkCancel", sellerLinkCancel);
+					.replaceAll("@lotId", preDeal.getLotId() + "")
+					.replaceAll("@lotDate", lot.getDateStr())
+					.replaceAll("@lotPrice", lot.getMinPrice() + "")
+					.replaceAll("@price", price + "")
+					.replaceAll("@sellerLinkApprove", sellerLinkApprove)
+					.replaceAll("@sellerLinkCancel", sellerLinkCancel);
 
-			customerText = customerText.replaceAll("@tenderId", preDeal.getTenderId() + "");
-			customerText = customerText.replaceAll("@tenderDate", tender.getDateStr());
-			customerText = customerText.replaceAll("@tenderPrice", tender.getMaxPrice() + "");
-			customerText = customerText.replaceAll("@price", price + "");
-			customerText = customerText.replaceAll("@customerLinkApprove", customerLinkApprove);
-			customerText = customerText.replaceAll("@customerLinkCancel", customerLinkCancel);			
+			customerText = customerText
+					.replaceAll("@tenderId", preDeal.getTenderId() + "")
+					.replaceAll("@tenderDate", tender.getDateStr())
+					.replaceAll("@tenderPrice", tender.getMaxPrice() + "")
+					.replaceAll("@price", price + "")
+					.replaceAll("@customerLinkApprove", customerLinkApprove)
+					.replaceAll("@customerLinkCancel", customerLinkCancel);			
 			
 			lot.setStatus(Status.SUCCESS);
 			DaoFactory.getLotDao().updateById(lot);
@@ -76,7 +83,7 @@ public class EmailForConfirmation {
 			DaoFactory.getTenderDao().updateById(tender);
 		
 			new MailManager(EMAIL_USER, EMAIL_PASS).send(sellerEmail, sellerSubject, sellerText);
-			new MailManager(EMAIL_USER, EMAIL_PASS).send(customerEmail, customerSubject, customerText);			
-		}
-	}
+			new MailManager(EMAIL_USER, EMAIL_PASS).send(customerEmail, customerSubject, customerText);				
+		}	
+	}	
 }
