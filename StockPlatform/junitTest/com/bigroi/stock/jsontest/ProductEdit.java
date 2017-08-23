8f3157b2a0baca54593afb4ed606100914f8aacf
evgen <evgen@192.168.60.78>
2017-08-23 15:30:44 +0300

    ProductResourseController completed on 90%

---
+++ StockPlatform/junitTest/com/bigroi/stock/jsontest/ProductEdit.java
@@ -0,0 +1,31 @@
+package com.bigroi.stock.jsontest;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.bigroi.stock.bean.Product;
+import com.bigroi.stock.bean.ResultBean;
+import com.bigroi.stock.controller.ProductResourseController;
+import com.bigroi.stock.dao.DaoException;
+import com.google.gson.Gson;
+
+public class ProductEdit {
+	
+	private static Product product;
+	
+	@BeforeClass
+	public static void init(){
+		product = new Product();
+		product.setId(-1);
+	}
+	
+	@Test
+	public void  productEdit() throws DaoException{
+		ProductResourseController res = new ProductResourseController();
+		String result = res.productEdit(product.getId(), new Gson().toJson(product));
+		ResultBean bean = new Gson().fromJson(result, ResultBean.class);
+		Assert.assertEquals(bean.getResult(), 1);
+	}
+
+}
