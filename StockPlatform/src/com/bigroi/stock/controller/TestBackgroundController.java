f10090b21785c4a6903689acc163778736a0bbd5
Alexei <Alexei@DESKTOP-9QR6NU3>
2017-08-31 21:49:30 +0300

    Update test page with corrections to the remarks of Pavel

--- StockPlatform/src/com/bigroi/stock/controller/TestBackgroundController.java
+++ StockPlatform/src/com/bigroi/stock/controller/TestBackgroundController.java
@@ -1,8 +1,5 @@
 package com.bigroi.stock.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,34 +12,28 @@ import com.bigroi.stock.messager.SendEmail;
 public class TestBackgroundController extends ResourseBeanException {
 	
 	@RequestMapping(value = "/testBackgroundFunctions.spr")
-	public ModelAndView showPage(){
-		return new ModelAndView("testBackgroundFunctions");
+	public String showPage(){
+		return "test/testBackgroundFunctions";
 	} 
 
 	@RequestMapping(value = "/StartClearPreDeal.spr")
 	public ModelAndView startClearPredeal() {
 		new ClearPreDeal().run();
 		String message = "ClearPreDeal().run() is finished";
-		Map<String, Object> map = new HashMap<>();
-		map.put("message", message);
-		return new ModelAndView("testBackgroundFunctions", map);
+		return new ModelAndView("test/testBackgroundFunctions", "message", message);
 	}
  
 	@RequestMapping(value = "/StartTrade.spr")
 	public ModelAndView startTrade() {
 		new Trade().run();
 		String message = "Trade().run() is finished";
-		Map<String, Object> map = new HashMap<>();
-		map.put("message", message);
-		return new ModelAndView("testBackgroundFunctions", map);
+		return new ModelAndView("test/testBackgroundFunctions",  "message", message);
 	}
 
 	@RequestMapping(value = "/StartSendEmail.spr")
 	public ModelAndView startSendMail() {
 		new SendEmail().run();
 		String message = "SendEmail().run() is finished";
-		Map<String, Object> map = new HashMap<>();
-		map.put("message", message);
-		return new ModelAndView("testBackgroundFunctions", map);
+		return new ModelAndView("test/testBackgroundFunctions",  "message", message);
 	}
 }
