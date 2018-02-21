616027268b81c42f87d133f3567a4997fa08e76c
Margarita <bitbucketM92>
2018-02-21 23:03:46 +0300

    Правка срипта для мобильной версии (выпадающее меню)

--- StockPlatform/WebContent/js/script.js
+++ StockPlatform/WebContent/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-		(".burger").click(function(){
+		$(".burger").click(function(){
 			  $(".aside nav.main-menu, .bgdark").fadeToggle();
 			  $(".section").toggleClass("section-hidden");
 			});
