282ec5b6512bcf349c5e0c44baad78d2d4aa57ac
Margarita <bitbucketM92>
2018-02-21 23:00:10 +0300

    Отмена скрипта отвечающего за то, что мобильной версии при развернутом меню -кликаешь в другое место и меню сворачивалось (его необходимо заменить на другой)

--- StockPlatform/WebContent/js/script.js
+++ StockPlatform/WebContent/js/script.js
@@ -1,14 +1,7 @@
 $(document).ready(function(){
-	if ($('body').width() <= 375) {
 		(".burger").click(function(){
 			  $(".aside nav.main-menu, .bgdark").fadeToggle();
 			  $(".section").toggleClass("section-hidden");
 			});
-			$(document).on('click', function(e) {
-				  if (!$(e.target).closest(".aside").length) {
-				    $('.aside nav.main-menu, .bgdark').fadeOut();
-				  }
-				  e.stopPropagation();
-			});	
-    }
+    
 });
