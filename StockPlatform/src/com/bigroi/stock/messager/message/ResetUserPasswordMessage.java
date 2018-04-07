95940c96f6646bf2a4813998da4c7b6cf2969266
Piniuta_P <Piniuta_P@PINIUTA-T470S.iba>
2018-04-07 16:01:59 +0300

    remove imports

--- StockPlatform/src/com/bigroi/stock/messager/message/ResetUserPasswordMessage.java
+++ StockPlatform/src/com/bigroi/stock/messager/message/ResetUserPasswordMessage.java
@@ -1,11 +1,6 @@
 package com.bigroi.stock.messager.message;
 
-import com.bigroi.stock.bean.Company;
 import com.bigroi.stock.bean.StockUser;
-import com.bigroi.stock.messager.MessagerFactory;
-import com.bigroi.stock.service.ServiceException;
-import com.bigroi.stock.service.ServiceFactory;
-import com.bigroi.stock.util.Generator;
 
 public class ResetUserPasswordMessage extends BaseMessage<StockUser> {
 
