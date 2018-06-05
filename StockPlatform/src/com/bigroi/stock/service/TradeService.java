028c50c30736922d14cf64440615d9f4c87c0b9b
Pavel <pinyuta-p@yandex.ru>
2018-06-05 20:08:16 +0300

    set trade bean as prototype

--- StockPlatform/src/com/bigroi/stock/service/TradeService.java
+++ StockPlatform/src/com/bigroi/stock/service/TradeService.java
@@ -1,8 +1,11 @@
 package com.bigroi.stock.service;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public interface TradeService {
 	
 	public void trade() throws ServiceException;
