be9f3f1d7b09251469dbf912df4ced889bcfd8b3
Margarita <bitbucketM92>
2018-07-03 22:22:14 +0300

    Верстка страниц с таблицами

--- StockStatic/WebContent/js/productList.js
+++ StockStatic/WebContent/js/productList.js
@@ -21,41 +21,63 @@ $(document).ready(function(){
 		
 		var sellProduct = $("<div class='sell-product'>");
 		sellProduct.append($("<h5>" + l10n.translate("label.index.sell") +"</h5>"));
-		sellProduct.append($("<p class='count'>" + product.sellVolume + "</p>"));
-		sellProduct.append($("<p class='desc-count'>" + l10n.translate("label.index.requests_volume") + "</p>"));
-		sellProduct.append($("<p class='count'>" + product.sellPrice + "</p>"));
-		sellProduct.append($("<p class='desc-count'>" + l10n.translate("label.index.average_price") + "</p>"));
-		if (!authorised){
+		var buyProduct = $("<div class='buy-product'>");
+		buyProduct.append($("<h5>" + l10n.translate("label.index.buy") +"</h5>"));
+		
+		if(!authorised){
+			sellProduct.append($("<p class='count'>" + product.sellVolume + "</p>"));
+			sellProduct.append($("<p class='desc-count'>" + l10n.translate("label.index.requests_volume") + "</p>"));
+			sellProduct.append($("<p class='count'>" + product.sellPrice + "</p>"));
+			sellProduct.append($("<p class='desc-count'>" + l10n.translate("label.index.average_price") + "</p>"));
+			aboutProduct.append(sellProduct);
+			buyProduct.append($("<p class='count'>" + product.buyVolume + "</p>"));
+			buyProduct.append($("<p class='desc-count'>" + l10n.translate("label.index.requests_volume") + "</p>"));
+			buyProduct.append($("<p class='count'>" + product.buyPrice + "</p>"));
+			buyProduct.append($("<p class='desc-count'>" + l10n.translate("label.index.average_price") + "</p>"));
+			aboutProduct.append(buyProduct);
+			
 			var sellButton = $("<button class='green-button'>" + l10n.translate("label.index.sell") + " " + product.name + "</button>");
 			sellProduct.append(sellButton);
 			sellButton.on("click", function(){
 				showDialog(getReginDialogParams());
 			});
-		}
-		aboutProduct.append(sellProduct);
-		
-		var buyProduct = $("<div class='buy-product'>");
-		buyProduct.append($("<h5>" + l10n.translate("label.index.buy") +"</h5>"));
-		buyProduct.append($("<p class='count'>" + product.buyVolume + "</p>"));
-		buyProduct.append($("<p class='desc-count'>" + l10n.translate("label.index.requests_volume") + "</p>"));
-		buyProduct.append($("<p class='count'>" + product.buyPrice + "</p>"));
-		buyProduct.append($("<p class='desc-count'>" + l10n.translate("label.index.average_price") + "</p>"));
-		if (!authorised){
+			
 			var buyButton = $("<button class='blue-button'>" + l10n.translate("label.index.buy") +" " + product.name + "</button>");
 			buyProduct.append(buyButton);
 			buyButton.on("click", function(){
 				showDialog(getReginDialogParams());
 			});
+			
 		}
-		aboutProduct.append(buyProduct);
-		div.append(aboutProduct);
 		if (authorised){
+			var tableProduct = $("<table>");
+			var tableTheadProduct = $("<thead><tr><td></td><td>" + l10n.translate("label.index.average_price") + "</td><td>" + l10n.translate("label.index.requests_volume") + "</td></tr></thead>");
+			tableProduct.append(tableTheadProduct);
+			var tableTbodyProduct = $("<tbody>");
+			tableProduct.append(tableTbodyProduct);
+			
+			var trSellProduct = $("<tr class='sell-tr'>");
+			tableTbodyProduct.append(trSellProduct);
+
+			trSellProduct.append($("<td>" + l10n.translate("label.index.sell") + "</td>"));
+			trSellProduct.append($("<td>" + product.sellPrice + "</td>"));
+			trSellProduct.append($("<td>" + product.sellVolume + "</td>"));
+			
+			var trBuyProduct = $("<tr class='buy-tr'>");
+			tableTbodyProduct.append(trBuyProduct);
+			trBuyProduct.append($("<td>" + l10n.translate("label.index.buy") + "</td>"));
+			trBuyProduct.append($("<td>" + product.buyPrice + "</td>"));
+			trBuyProduct.append($("<td>" + product.buyVolume + "</td>"));
+			
+			aboutProduct.append(tableProduct);
+			
 			var details = $("<button class='blue-button'>" + l10n.translate("label.product.details") +" " + product.name + "</button>");
 			div.append(details);
 			details.on("click", function(){
 				document.location = getContextRoot() + "/product/TradeOffers.spr?id=" + product.id;
 			});
 		}
+		div.append(aboutProduct);
 		container.append(div);
 	}
 });
