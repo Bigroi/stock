345ba353d0d491bb65e134651cdf495b7b70b1fc
Yauheni <javadev6891@gmail.com>
2018-07-22 18:52:50 +0300

    AddressServiceImplTest mockito

---
+++ StockService/test/com/bigroi/stock/service/impl/AddressServiceImplTest.java
@@ -0,0 +1,130 @@
+package com.bigroi.stock.service.impl;
+
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.bigroi.stock.bean.db.CompanyAddress;
+import com.bigroi.stock.dao.AddressDao;
+import com.bigroi.stock.dao.DaoException;
+import com.bigroi.stock.service.ServiceException;
+import com.bigroi.stock.util.BaseTest;
+import com.google.common.collect.ImmutableList;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AddressServiceImplTest extends BaseTest {
+
+	@InjectMocks
+	private AddressServiceImpl addressService;
+	@Mock
+	private AddressDao addressDao;
+	
+	private Random random = new Random();
+
+	@Test
+	public void getCompanyAddressesTest() throws ServiceException, DaoException {
+		// given
+		final long COMPANY_ID = random.nextLong();
+		//mock
+		CompanyAddress company = new CompanyAddress();
+		company.setCompanyId(COMPANY_ID);
+		List<CompanyAddress> expectedList = ImmutableList.of(company);
+		Mockito.when(addressDao.getAddressesForCompany(COMPANY_ID)).thenReturn(expectedList);
+		// when
+		List<CompanyAddress> actualList = addressService.getCompanyAddresses(COMPANY_ID);
+		// then
+		Assert.assertEquals(expectedList, actualList);
+		Mockito.verify(addressDao, Mockito.times(1)).getAddressesForCompany(COMPANY_ID);
+	}
+
+	@Test
+	public void getAddressByIdTest() throws DaoException, ServiceException {
+		// given
+		final long ADDRESS_ID = random.nextLong();
+		final long COMPANY_ID = random.nextLong();
+		//mock
+		CompanyAddress expectedAddress = new CompanyAddress();
+		expectedAddress.setCompanyId(COMPANY_ID);
+		Mockito.when(addressDao.getAddressById(ADDRESS_ID, COMPANY_ID)).thenReturn(expectedAddress);
+		// when
+		CompanyAddress actualAddress = addressService.getAddressById(ADDRESS_ID, COMPANY_ID);
+		CompanyAddress actualNewAddress = addressService.getAddressById(-1, COMPANY_ID);
+		// then
+		Assert.assertEquals(expectedAddress, actualAddress);
+		Assert.assertEquals(-1, (int) actualNewAddress.getLatitude());
+		Assert.assertEquals(-1, (int) actualNewAddress.getLongitude());
+		Assert.assertEquals(-1, actualNewAddress.getId());
+		Mockito.verify(addressDao, Mockito.times(1)).getAddressById(ADDRESS_ID, COMPANY_ID);
+	}
+	
+	@Test
+	public void mergeAddTest() throws DaoException, ServiceException {
+		// given
+		final long COMPANY_ID = random.nextLong();
+		CompanyAddress newAddress = createObject(CompanyAddress.class);
+		newAddress.setId(-1);
+		//mock
+		Mockito.doAnswer(a -> {
+								((CompanyAddress)a.getArguments()[0]).setId(random.nextLong()); 
+								return null;
+								})
+			.when(addressDao).addAddress(newAddress);
+		// when
+		addressService.merge(newAddress, COMPANY_ID);
+		// then
+		Assert.assertEquals(COMPANY_ID, newAddress.getCompanyId());
+		Assert.assertNotEquals(-1, newAddress.getId());
+		Mockito.verify(addressDao, Mockito.times(1)).addAddress(newAddress);
+	}
+	
+	@Test
+	public void mergeUpdateTest() throws DaoException, ServiceException {
+		// given
+		final long COMPANY_ID = random.nextLong();
+		CompanyAddress updateAddress = new CompanyAddress();
+		//mock
+		Mockito.doAnswer(a -> {
+								((CompanyAddress)a.getArguments()[0]).setId(random.nextLong()); 
+								return null;
+								})
+			.when(addressDao).updateAddress(updateAddress);
+		// when
+		addressService.merge(updateAddress, COMPANY_ID);
+		// then
+		Assert.assertEquals(COMPANY_ID, updateAddress.getCompanyId());	
+		Mockito.verify(addressDao, Mockito.times(1)).updateAddress(updateAddress);
+	}
+	
+	@Test
+	public void deleteTest() throws DaoException, ServiceException {
+		// given
+		final long ADDRESS_ID = random.nextLong();
+		final long COMPANY_ID = random.nextLong();
+		//mock
+		Mockito.when(addressDao.deleteAddress(ADDRESS_ID, COMPANY_ID)).thenReturn(true);
+		// when
+		addressService.delete(ADDRESS_ID, COMPANY_ID);
+		// then
+		Mockito.verify(addressDao, Mockito.times(1)).deleteAddress(ADDRESS_ID, COMPANY_ID);
+	}
+	
+	@Test
+	public void hasAddressTest() throws DaoException, ServiceException {
+		// given
+		final long COMPANY_ID = random.nextLong();
+		CompanyAddress hasAddress = new CompanyAddress();
+		//mock
+		Mockito.when(addressDao.hasAddress(hasAddress, COMPANY_ID)).thenReturn(true);
+		// when
+		addressService.hasAddress(hasAddress, COMPANY_ID);
+		// then
+		Mockito.verify(addressDao, Mockito.times(1)).hasAddress(hasAddress, COMPANY_ID);
+	}
+}
---
+++ StockUtil/test/com/bigroi/stock/util/BaseTest.java
@@ -0,0 +1,46 @@
+package com.bigroi.stock.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public abstract class BaseTest {
+	
+	private Random random = new Random();
+	
+	protected  <T> T createObject(Class<T> clazz){
+		try {
+			T object = clazz.newInstance();
+			for(Field field : clazz.getDeclaredFields()){
+				if(Modifier.isFinal(field.getModifiers()) || Modifier.isStatic(field.getModifiers()) ){
+					continue;
+				}
+				if(field.getType().equals(int.class)){
+					setField(random::nextInt, field, object);
+				}else if(field.getType().equals(long.class)){
+					setField(random::nextLong, field, object);
+				}else if(field.getType().equals(double.class)){
+					setField(random::nextDouble, field, object);
+				}else if(field.getType().equals(byte.class)){
+					setField(random::nextInt, field, object);
+				}else if(field.getType().equals(short.class)){
+					setField(random::nextInt, field, object);
+				}else if(field.getType().equals(float.class)){
+					setField(random::nextFloat, field, object);
+				}else if(field.getType().equals(char.class)){
+					setField(random::nextInt, field, object);
+				}
+			}
+			return object;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private void setField(Callable<?> funnction, Field field, Object object) throws Exception{
+		field.setAccessible(true);
+		field.set(object, funnction.call());
+		field.setAccessible(false);
+	}
+}
