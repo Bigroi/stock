372eb13507c3c7e7ed3586faaa95457cf01b1704
Margarita <bitbucketM92>
2018-09-18 14:15:54 +0300

    Флаги для переключателя языка

--- StockStatic/WebContent/js/script.js
+++ StockStatic/WebContent/js/script.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
+	
+	$( function() {
+	    $.widget( "custom.iconselectmenu", $.ui.selectmenu, {
+	      _renderItem: function( ul, item ) {
+	        var li = $( "<li>" ),
+	          wrapper = $( "<div>", { text: item.label } );
+	        if ( item.disabled ) {li.addClass( "ui-state-disabled" );}
+	        $( "<span>", {style: item.element.attr( "data-style" ),"class": "ui-icon " + item.element.attr( "data-class" )
+	        }).appendTo( wrapper );
+	        return li.append( wrapper ).appendTo( ul );
+	      }
+	    });
+	    $("#languages-select").iconselectmenu().iconselectmenu("menuWidget").addClass("ui-menu-icons customicons");
+	    var selectedImage = $(this).find("option:selected").attr("data-class");
+	    $(".ui-selectmenu-text").css('background-image', 'url("/Static/img/' + selectedImage +'.png")');
+	    $( "#languages-select" ).iconselectmenu({
+	    	select: function(event, ui){ 
+		        var selectedImage = $(this).find("option:selected").attr("data-class");
+		        $(".ui-selectmenu-text").css('background-image', 'url("/Static/img/' + selectedImage +'.png")');
+	    	}
+	    });
+	});
+	
 	$(document).on("click", '#continue', function() {
 		if($('#registration-form')[0].checkValidity()){
 			$(".registration-first-part").hide();
