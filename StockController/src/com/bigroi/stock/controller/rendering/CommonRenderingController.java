9e92222320d510954d44483460beb3fae49dba38
pinyu <pinyu@PINIUTA>
2018-10-24 02:28:43 +0300

    404 and 500 error pages

--- StockController/src/com/bigroi/stock/controller/rendering/CommonRenderingController.java
+++ StockController/src/com/bigroi/stock/controller/rendering/CommonRenderingController.java
@@ -17,28 +17,38 @@ import com.bigroi.stock.service.UserService;
 import com.bigroi.stock.util.LabelUtil;
 
 @Controller
-public class CommonRenderingController extends BaseRenderingController{
+public class CommonRenderingController extends BaseRenderingController {
 
 	private static final String RESET_PASSW_PAGE = "resetPassw";
 
 	@Autowired
 	private UserService userService;
-	
+
+	@RequestMapping("404.spr")
+	public ModelAndView handle404() {
+		return createModelAndView("404");
+	}
+
+	@RequestMapping("500.spr")
+	public ModelAndView handle500() {
+		return createModelAndView("500");
+	}
+
 	@RequestMapping("/Login.spr")
 	public ModelAndView login() {
 		return createModelAndView("login");
 	}
-	
+
 	@RequestMapping("/AnotherBrowser.spr")
 	public ModelAndView anotherBrowser() {
 		return createModelAndView("anotherBrowser");
 	}
-	
+
 	@RequestMapping("/Index.spr")
 	public ModelAndView index(HttpServletRequest request, Authentication loggedInUser) {
-		if (loggedInUser != null){ 
+		if (loggedInUser != null) {
 			Object user = loggedInUser.getPrincipal();
-			if (user instanceof StockUser){
+			if (user instanceof StockUser) {
 				return new ModelAndView("redirect:" + AuthenticationHandler.getMainPage(request.getContextPath()));
 			}
 		}
@@ -49,14 +59,14 @@ public class CommonRenderingController extends BaseRenderingController{
 	public ModelAndView goToRegistrationPage() {
 		return createModelAndView("registration");
 	}
-	
+
 	@RequestMapping(value = "/ResetForm.spr")
 	public ModelAndView resetForm() {
 		return createModelAndView("resetForm");
 	}
 
 	@RequestMapping(value = "/ResetPassword.spr")
-	public ModelAndView resetPassword(@RequestParam("code") String code, @RequestParam("email") String email){
+	public ModelAndView resetPassword(@RequestParam("code") String code, @RequestParam("email") String email) {
 		ModelAndView modelAndView = createModelAndView(RESET_PASSW_PAGE);
 		String message;
 		if (userService.changePassword(email, code)) {
@@ -64,12 +74,10 @@ public class CommonRenderingController extends BaseRenderingController{
 		} else {
 			message = labelService.getLabel(RESET_PASSW_PAGE, "error", getLanguage());
 		}
-		modelAndView
-			.addObject("message", message)
-			.addObject("user", null);
+		modelAndView.addObject("message", message).addObject("user", null);
 		return modelAndView;
 	}
-	
+
 	@RequestMapping("/account/Form.spr")
 	@Secured(value = { "ROLE_USER", "ROLE_ADMIN" })
 	public ModelAndView form() {
