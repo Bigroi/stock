2f2a7d13c38fc2bb6ba3662a82d1d24099a4077a
pinyu <pinyu@PINIUTA>
2018-10-29 02:57:37 +0300

    contract document

--- StockDocuments/src/com/bigroi/stock/docs/DealDocument.java
+++ StockDocuments/src/com/bigroi/stock/docs/DealDocument.java
@@ -16,7 +16,7 @@ import com.bigroi.stock.util.exception.StockRuntimeException;
 public class DealDocument extends Document<Deal>{
 
 	public static final String DEAL_DOC_FILE_NAME = "Deal";
-	public static final String DEAL_DOC_FILE_EXTENSION = "doc";
+	public static final String DEAL_DOC_FILE_EXTENSION = "docx";
 	
 	public DealDocument(){
 		super(DEAL_DOC_FILE_NAME, DEAL_DOC_FILE_EXTENSION);
@@ -25,25 +25,19 @@ public class DealDocument extends Document<Deal>{
 	public byte[] getDocument(Deal deal) {
 		try {
 			Map<String, Object> map = new HashMap<>();
-			map.put("{date}", new SimpleDateFormat("dd.MM.yyyy").format(new Date()));
-			map.put("{seller}", deal.getSellerAddress().getCompany().getName());
-			map.put("{seller_person}", deal.getSellerAddress().getCompany().getEmail());
-			map.put("{buyer}", deal.getBuyerAddress().getCompany().getName());
-			map.put("{buyer_person}", deal.getBuyerAddress().getCompany().getEmail());
-			map.put("{volume}", deal.getVolume());
-			map.put("{price}", deal.getPrice());
-			map.put("{product}", deal.getProduct().getName());
-			map.put("{seller_reg_number}", deal.getSellerAddress().getCompany().getRegNumber());
+			map.put("{product_name}", deal.getProduct().getName());
+			map.put("{deal_date}", new SimpleDateFormat("dd.MM.yyyy").format(new Date()));
+			map.put("{seller_city}", deal.getSellerAddress().getCity());
+			map.put("{seller_company_name}", deal.getSellerAddress().getCompany().getName());
+			map.put("{seller_company_reg_number}", deal.getSellerAddress().getCompany().getRegNumber());
+			map.put("{seller_address}", deal.getSellerAddress().getAddress());
+			map.put("{buyer_company_name}", deal.getBuyerAddress().getCompany().getName());
+			map.put("{buyer_company_reg_number}", deal.getBuyerAddress().getCompany().getRegNumber());
+			map.put("{buyer_city}", deal.getBuyerAddress().getCity());
+			map.put("{buyer_address}", deal.getBuyerAddress().getAddress());
+			map.put("{deal_volume}", deal.getVolume());
 			map.put("{seller_phone}", deal.getSellerAddress().getCompany().getPhone());
-			map.put("{seller_address}", deal.getSellerAddress().getCountry() + " " +
-										deal.getSellerAddress().getCity() + " " +
-										deal.getSellerAddress().getAddress());
 			
-			map.put("{buyer_reg_number}", deal.getBuyerAddress().getCompany().getRegNumber());
-			map.put("{buyer_phone}", deal.getBuyerAddress().getCompany().getPhone());
-			map.put("{buyer_address}", deal.getBuyerAddress().getCountry() + " " +
-										deal.getBuyerAddress().getCity() + " " +
-										deal.getBuyerAddress().getAddress());
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			
 			String language = deal.getSellerAddress().getCompany().getLanguage();
