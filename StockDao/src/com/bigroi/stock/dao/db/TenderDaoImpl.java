bbd020dfbe47e207b5139b841b1d91d468f9985d
pinyu <pinyu@PINIUTA>
2018-11-01 22:07:25 +0300

    labels

--- StockDao/src/com/bigroi/stock/dao/db/TenderDaoImpl.java
+++ StockDao/src/com/bigroi/stock/dao/db/TenderDaoImpl.java
@@ -5,7 +5,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.SQLType;
 import java.sql.Types;
 import java.util.Collection;
 import java.util.List;
